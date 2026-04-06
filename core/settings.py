9737f12707107f1244fe59c2dc9b69bc52844d60
luiz bezerra <lbmaxlc@gmail.com>
2026-04-06 20:14:20 -0300

    Fix: Forçando conexão com banco de dados externo

--- core/settings.py
+++ core/settings.py
@@ -81,23 +81,18 @@ REST_FRAMEWORK = {
 }
 
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+db_url = os.environ.get('DATABASE_URL')
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=db_url,
         conn_max_age=600,
         conn_health_checks=True,
-    )
-}
-
-if not DATABASES['default']:
-    DATABASES['default'] = {
+    ) if db_url else {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
